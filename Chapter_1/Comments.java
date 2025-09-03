f6254e77596992cfec74fd6a1846dc8392510313
ZoeJadonCadiz-UCM <91902300+ZoeJadonCadiz-UCM@users.noreply.github.com>
2025-09-03 19:58:54 +0800

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,22 @@
+
+public class Comments {
+
+    public static void main(String[] args) {
+
+        // Single-line comment
+        System.out.println("This text is for the single line comment");
+
+        /*
+         * Multi-line comment:
+         * The next line of code prints another message.
+         * Multi-line comments are useful for longer explanations.
+         */
+        System.out.println("This text is an example of multi-line comments");
+
+        /**
+         * This is a document comment
+         */
+        System.out.println ("This is for documentation comment");
+
+    }
+}
