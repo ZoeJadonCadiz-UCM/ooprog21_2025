89b14a0cdc62d9f1ac27113ba715dc95d8be5c6d
ZoeJadonCadiz-UCM <91902300+ZoeJadonCadiz-UCM@users.noreply.github.com>
2025-09-03 19:51:24 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+
+    public static void main (String []args) {
+
+        System.out.println ("Di ko maiwasang pagmasdan ang 'yong larawan");
+        System.out.println ("Kahit di mo mamalayan, ikaw lang ang gustong tignan");
+        System.out.println ("Di ko man masabi, kahit na 'di maaari");
+        System.out.println ("Pwede ba 'kong manatili? Ikaw lang ang gustong pagmasdan");
+    }
+    
+}
