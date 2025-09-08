e06f3172e3377693de7f159f68f7e8d7e8346b43
ZoeJadonCadiz-UCM <91902300+ZoeJadonCadiz-UCM@users.noreply.github.com>
2025-09-08 22:13:40 +0800

    Create FixDebugOne.java

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,21 @@
+
+public class FixDebugOne {
+
+    /* This program displays a greeting */
+    public static void main (String [] args) {
+
+        System.out.println ("Hello World!");
+    }
+}
+
+/*
+ * List down the spotted bugs below:
+ * 
+ * Bugs:
+ * 1. The class name is invalid as it shouldn't contain spaces. It should be one as FixDebugOne
+ * 2. Main method shouldn't be capitalized. It should be public static void main, not Main.
+ * 3. There is no array [] in public static void main (String args)
+ * 4. Typo in Systm.out.println (It should be complete as System.out.println)
+ * 5. There is no semicolon after the java statement System.out.println
+ * 6. There is no closed curly bracket for the class name.
+ */
