5e947beb7600bb0c1b93087932edb3a9ac3018e0
ZoeJadonCadiz-UCM <91902300+ZoeJadonCadiz-UCM@users.noreply.github.com>
2025-09-24 08:09:06 +0800

    Create DebugCircle.java

---
+++ Chapter_4/DebugCircle.java
@@ -0,0 +1,40 @@
+
+public class DebugCircle
+{
+
+    public static void main(String[] args)
+    {
+        DebugCircle circle = new DebugCircle(5); // sample radius of circle (it can be changed)
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Diameter: " + circle.getDiameter());
+        System.out.println("Area: " + circle.getArea());
+    }
+    
+    private int radius;
+    private int diameter;
+    private final double PI = 3.14159;
+    private double area;
+
+    public DebugCircle(int r)
+    {
+        radius = r;
+        diameter = 2 * r;
+        area = PI * r * r;
+    }
+
+    public int getRadius()
+    {
+        return radius;
+    }
+
+    public int getDiameter()
+    {
+        return diameter;
+    }
+
+    public double getArea()
+    {
+        return area;
+    }
+
+}
